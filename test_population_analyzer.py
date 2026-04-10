07eb561acd2a2d0c895f8e22e1cd88d3f5e2460c
Kira Mikhoparova <mihoparovakira@gmail.com>
2026-04-10 15:55:35 +0300

    Add analyze func test

--- test_population_analyzer.py
+++ test_population_analyzer.py
@@ -21,3 +21,21 @@ def create_test_file(tmp_path):
 def test_read_population_data(create_test_file, content, expected):
     file_path = create_test_file(content)
     assert read_population_data(file_path) == expected
+
+
+@pytest.mark.parametrize("input_data, expected_changes", [
+    (
+        {"Ukraine": {2020: 41000, 2021: 40000}},
+        {"Ukraine": {"2020-2021": -1000}}
+    ),
+    (
+        {"USA": {2010: 300, 2020: 330}},
+        {"USA": {"2010-2020": 30}}
+    ),
+    (
+        {"Norway": {2021: 5000}},
+        {"Norway": {}}
+    )
+])
+def test_calculate_population_change(input_data, expected_changes):
+    assert calculate_population_change(input_data) == expected_changes
